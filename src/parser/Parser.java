99188aa7c225029b88704a41d4bb6cdaefbb7e92
sultazat <sultazat@gmail.com>
2018-12-06 17:36:46 +0300

    changes

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,12 +1,10 @@
 package parser;
 
 import d_grammar.DLexer;
-import org.antlr.v4.codegen.model.decl.Decl;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.TerminalNodeImpl;
 import scope.Scope;
-import types.EmptyValue;
 
 public class Parser {
     private ParseTree tree;
@@ -57,7 +55,7 @@ public class Parser {
             String name = tree.getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
             System.out.println(name);
             switch (name) {
-
+                
             }
         }
         return new Empty();
