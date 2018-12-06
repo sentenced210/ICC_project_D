acdbce2b38b28a71c4f7b7c5ab119ef7b15a9979
Amir Nazyrov <a.nazyroff@mail.ru>
2018-12-06 19:53:10 +0300

    Adding own error listener on the parser

--- src/main/main/Main.java
+++ src/main/main/Main.java
@@ -3,6 +3,7 @@ package main.main;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 import d_grammar.DLexer;
@@ -10,6 +11,7 @@ import d_grammar.DParser;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.text.ParseException;
 
 import parser.Parser;
 
@@ -20,21 +22,38 @@ public class Main {
         CharStream stream = CharStreams.fromFileName("test");
 
         DLexer lexer = new DLexer(stream);
+
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
         DParser parser = new DParser(tokens);
-        ParseTree tree = parser.program();
 
-        ScopeChecker sc = new ScopeChecker();
-        sc.check(tree);
+        SyntaxErrorListener syntaxErrorListener = new SyntaxErrorListener();
 
-        String s = JsonCreater.toJson(tree);
-        PrintWriter pw = new PrintWriter(new File("ast.json"));
-        pw.write(s);
-        pw.close();
+        parser.removeErrorListeners();
+        parser.addErrorListener(syntaxErrorListener.INSTANCE);
+        try {
+            ParseTree tree = parser.program();
+        } catch (ParseCancellationException e){
+            System.out.println(e.getMessage());
+        }
 
-        Parser p = new Parser(tree);
-        p.parse();
+//        if (syntaxErrorListener.hasErrors()){
+//            System.out.println(syntaxErrorListener.errorMessages());
+//        }
+
+
+//        String s = JsonCreater.toJson(tree);
+//        PrintWriter pw = new PrintWriter(new File("ast.json"));
+//        pw.write(s);
+//        pw.close();
+
+
+//        ScopeChecker sc = new ScopeChecker();
+//        sc.check(tree);
+//
+//
+//        Parser p = new Parser(tree);
+//        p.parse();
     }
 
 }
