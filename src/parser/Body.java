317630452e9dc4f3f9e3f71325bf85dc3d4a594f
sultazat <sultazat@gmail.com>
2018-12-07 06:40:12 +0300

    infinite recursion fixed

--- src/parser/Body.java
+++ src/parser/Body.java
@@ -8,21 +8,37 @@ import java.util.ArrayList;
 
 public class Body {
     private ArrayList<Statement> statements;
+    private Body parentBody;
+    private boolean needToReturn = false;
+    private Value returnValue;
 
-    public Body() {
+    public Body(Body parentBody) {
         statements = new ArrayList<>();
+        this.parentBody = parentBody;
     }
 
     public void add(Statement s) {
         statements.add(s);
     }
 
+    public void returnFromBody(Value returnValue) {
+        if (parentBody != null) {
+            parentBody.returnFromBody(returnValue);
+        }
+        this.returnValue = returnValue;
+        needToReturn = true;
+    }
+
     public Value execute(Scope scope) throws Exception {
         for (Statement s : statements) {
             if (s instanceof ReturnStatement) {
-                return s.execute(scope);
+                returnFromBody(s.execute(scope));
             } else {
                 s.execute(scope);
+            }
+            if (needToReturn) {
+                needToReturn = false;
+                return returnValue;
             }
         }
         return new EmptyValue();
--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -15,81 +15,81 @@ public class Parser {
     }
 
     public Body parse() throws Exception {
-        return parseBody(tree.getChild(0));
+        return parseBody(tree.getChild(0), null);
     }
 
-    private Body parseBody(ParseTree tree) throws Exception {
-        Body b = new Body();
+    private Body parseBody(ParseTree tree, Body parentBody) throws Exception {
+        Body b = new Body(parentBody);
         for (int i = 0; i < tree.getChildCount(); i++) {
-            b.add(parseStatement(tree.getChild(i).getChild(0)));
+            b.add(parseStatement(tree.getChild(i).getChild(0), b));
         }
         return b;
     }
 
-    private Statement parseStatement(ParseTree tree) throws Exception {
+    private Statement parseStatement(ParseTree tree, Body parentBody) throws Exception {
         String name = tree.getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
         // System.out.println(name);
         switch (name) {
             case "declaration": {
                 final String varName = tree.getChild(1).getChild(0).getText();
-                final Expression exp = tree.getChild(1).getChildCount() < 3 ? new Empty() : parseExpression(tree.getChild(1).getChild(2));
+                final Expression exp = tree.getChild(1).getChildCount() < 3 ? new Empty() : parseExpression(tree.getChild(1).getChild(2), parentBody);
                 return new Declaration(varName, exp);
             }
             case "print": {
                 ArrayList<Expression> exps = new ArrayList<>();
                 for (int i = 1; i < tree.getChildCount(); i += 2) {
-                    exps.add(parseExpression(tree.getChild(i)));
+                    exps.add(parseExpression(tree.getChild(i), parentBody));
                 }
                 return new PrintFunction(exps);
             }
             case "if_statement": {
-                Expression condition = parseExpression(tree.getChild(1));
-                Body body = parseBody(tree.getChild(3));
-                Body elseBody = tree.getChildCount() > 5 ? parseBody(tree.getChild(5)) : null;
+                Expression condition = parseExpression(tree.getChild(1), parentBody);
+                Body body = parseBody(tree.getChild(3), parentBody);
+                Body elseBody = tree.getChildCount() > 5 ? parseBody(tree.getChild(5), parentBody) : null;
                 return new IFStatement(condition, body, elseBody);
             }
             case "while_loop": {
-                Expression condition = parseExpression(tree.getChild(1));
-                Body body = parseBody(tree.getChild(3));
+                Expression condition = parseExpression(tree.getChild(1), parentBody);
+                Body body = parseBody(tree.getChild(3), parentBody);
                 return new WhileLoop(condition, body);
             }
             case "for_loop": {
                 final String varName = tree.getChild(1).getText();
-                Expression left = parseExpression(tree.getChild(3));
-                Expression right = parseExpression(tree.getChild(5));
-                Body body = parseBody(tree.getChild(7));
+                Expression left = parseExpression(tree.getChild(3), parentBody);
+                Expression right = parseExpression(tree.getChild(5), parentBody);
+                Body body = parseBody(tree.getChild(7), parentBody);
                 return new ForLoop(varName, left, right, body);
             }
             case "assignment": {
                 String varName = tree.getChild(0).getChild(0).getText();
-                Expression exp = parseExpression(tree.getChild(2));
+                Expression exp = parseExpression(tree.getChild(2), parentBody);
                 return new Assignment(varName, exp);
             }
             case "return_statement": {
-                return new ReturnStatement(tree.getChildCount() == 1 ? null : parseExpression(tree.getChild(1)));
+                return new ReturnStatement(tree.getChildCount() == 1 ? null : parseExpression(tree.getChild(1), parentBody));
             }
             default:
                 return new Declaration("nothing", new Empty());
         }
     }
 
-    private Expression parseExpression(ParseTree tree) throws Exception {
+    private Expression parseExpression(ParseTree tree, Body parentBody) throws Exception {
         String name = tree.getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
         // System.out.println(name);
-        Expression exp = parseRelation(tree.getChild(0));
+        Expression exp = parseRelation(tree.getChild(0), parentBody);
         for (int i = 2; i < tree.getChildCount(); i += 2) {
             final String type = ((TerminalNodeImpl) tree.getChild(i - 1)).getSymbol().getText();
             switch (type) {
                 case "or": {
-                    exp = new OR(exp, parseRelation(tree.getChild(i)));
+                    exp = new OR(exp, parseRelation(tree.getChild(i), parentBody));
                     break;
                 }
                 case "and": {
-                    exp = new AND(exp, parseRelation(tree.getChild(i)));
+                    exp = new AND(exp, parseRelation(tree.getChild(i), parentBody));
                     break;
                 }
                 case "xor": {
-                    exp = new XOR(exp, parseRelation(tree.getChild(i)));
+                    exp = new XOR(exp, parseRelation(tree.getChild(i), parentBody));
                     break;
                 }
             }
@@ -97,35 +97,35 @@ public class Parser {
         return exp;
     }
 
-    private Expression parseRelation(ParseTree tree) throws Exception {
+    private Expression parseRelation(ParseTree tree, Body parentBody) throws Exception {
         String name = tree.getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
         // System.out.println(name);
-        Expression exp = parseFactor(tree.getChild(0));
+        Expression exp = parseFactor(tree.getChild(0), parentBody);
         for (int i = 2; i < tree.getChildCount(); i += 2) {
             final String type = ((TerminalNodeImpl) tree.getChild(i - 1)).getSymbol().getText();
             switch (type) {
                 case "<": {
-                    exp = new Less(exp, parseFactor(tree.getChild(i)));
+                    exp = new Less(exp, parseFactor(tree.getChild(i), parentBody));
                     break;
                 }
                 case "<=": {
-                    exp = new LessOrEqual(exp, parseFactor(tree.getChild(i)));
+                    exp = new LessOrEqual(exp, parseFactor(tree.getChild(i), parentBody));
                     break;
                 }
                 case ">": {
-                    exp = new Greater(exp, parseFactor(tree.getChild(i)));
+                    exp = new Greater(exp, parseFactor(tree.getChild(i), parentBody));
                     break;
                 }
                 case ">=": {
-                    exp = new GreaterOrEqual(exp, parseFactor(tree.getChild(i)));
+                    exp = new GreaterOrEqual(exp, parseFactor(tree.getChild(i), parentBody));
                     break;
                 }
                 case "=": {
-                    exp = new Equal(exp, parseFactor(tree.getChild(i)));
+                    exp = new Equal(exp, parseFactor(tree.getChild(i), parentBody));
                     break;
                 }
                 case "/=": {
-                    exp = new NotEqual(exp, parseFactor(tree.getChild(i)));
+                    exp = new NotEqual(exp, parseFactor(tree.getChild(i), parentBody));
                     break;
                 }
             }
@@ -133,19 +133,19 @@ public class Parser {
         return exp;
     }
 
-    private Expression parseFactor(ParseTree tree) throws Exception {
+    private Expression parseFactor(ParseTree tree, Body parentBody) throws Exception {
         String name = tree.getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
         // System.out.println(name);
-        Expression exp = parseTerm(tree.getChild(0));
+        Expression exp = parseTerm(tree.getChild(0), parentBody);
         for (int i = 2; i < tree.getChildCount(); i += 2) {
             final String type = ((TerminalNodeImpl) tree.getChild(i - 1)).getSymbol().getText();
             switch (type) {
                 case "+": {
-                    exp = new Add(exp, parseTerm(tree.getChild(i)));
+                    exp = new Add(exp, parseTerm(tree.getChild(i), parentBody));
                     break;
                 }
                 case "-": {
-                    exp = new Subtract(exp, parseTerm(tree.getChild(i)));
+                    exp = new Subtract(exp, parseTerm(tree.getChild(i), parentBody));
                     break;
                 }
             }
@@ -153,19 +153,19 @@ public class Parser {
         return exp;
     }
 
-    private Expression parseTerm(ParseTree tree) throws Exception {
+    private Expression parseTerm(ParseTree tree, Body parentBody) throws Exception {
         String name = tree.getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
         // System.out.println(name);
-        Expression exp = parseUnary(tree.getChild(0));
+        Expression exp = parseUnary(tree.getChild(0), parentBody);
         for (int i = 2; i < tree.getChildCount(); i += 2) {
             final String type = ((TerminalNodeImpl) tree.getChild(i - 1)).getSymbol().getText();
             switch (type) {
                 case "*": {
-                    exp = new Multiply(exp, parseUnary(tree.getChild(i)));
+                    exp = new Multiply(exp, parseUnary(tree.getChild(i), parentBody));
                     break;
                 }
                 case "/": {
-                    exp = new Divide(exp, parseUnary(tree.getChild(i)));
+                    exp = new Divide(exp, parseUnary(tree.getChild(i), parentBody));
                     break;
                 }
             }
@@ -173,28 +173,28 @@ public class Parser {
         return exp;
     }
 
-    private Expression parseUnary(ParseTree tree) throws Exception {
+    private Expression parseUnary(ParseTree tree, Body parentBody) throws Exception {
         String name = tree.getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
         // System.out.println(name);
         switch (tree.getChildCount()) {
             case 1: {
                 String childName = tree.getChild(0).getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
                 if (childName.equals("reference")) {
-                    return parseReference(tree.getChild(0));
+                    return parseReference(tree.getChild(0), parentBody);
                 } else {
-                    return parsePrimary(tree.getChild(0));
+                    return parsePrimary(tree.getChild(0), parentBody);
                 }
             }
             case 2: {
                 switch (tree.getChild(0).getText()) {
                     case "+": {
-                        return new Add(new Integer(0), parsePrimary(tree.getChild(1)));
+                        return new Add(new Integer(0), parsePrimary(tree.getChild(1), parentBody));
                     }
                     case "-": {
-                        return new Subtract(new Integer(0), parsePrimary(tree.getChild(1)));
+                        return new Subtract(new Integer(0), parsePrimary(tree.getChild(1), parentBody));
                     }
                     case "not": {
-                        return new NOT(parsePrimary(tree.getChild(1)));
+                        return new NOT(parsePrimary(tree.getChild(1), parentBody));
                     }
                 }
             }
@@ -205,7 +205,7 @@ public class Parser {
         return new Empty();
     }
 
-    private Expression parseReference(ParseTree tree) throws Exception {
+    private Expression parseReference(ParseTree tree, Body parentBody) throws Exception {
         String name = tree.getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
         // System.out.println(name);
         if (tree.getChildCount() == 1) {
@@ -214,31 +214,31 @@ public class Parser {
         else {
             switch (tree.getChild(1).getText()) {
                 case "[": {
-                    return new ArrayReference(parseReference(tree.getChild(0)), parseExpression(tree.getChild(2)));
+                    return new ArrayReference(parseReference(tree.getChild(0), parentBody), parseExpression(tree.getChild(2), parentBody));
                 }
                 case "(": {
                     ArrayList<Expression> exps = new ArrayList<>();
                     for (int i = 2; i < tree.getChildCount(); i += 2) {
-                        exps.add(parseExpression(tree.getChild(i)));
+                        exps.add(parseExpression(tree.getChild(i), parentBody));
                     }
-                    return new FunctionReference(parseReference(tree.getChild(0)), exps);
+                    return new FunctionReference(parseReference(tree.getChild(0), new Body(null)), exps);
                 }
                 case "." : {
-                    return new TupleReference(parseReference(tree.getChild(0)), new IDENT(tree.getChild(2).getText()));
+                    return new TupleReference(parseReference(tree.getChild(0), parentBody), new IDENT(tree.getChild(2).getText()));
                 }
             }
         }
         return new Empty();
     }
 
-    private Expression parsePrimary(ParseTree tree) throws Exception {
+    private Expression parsePrimary(ParseTree tree, Body parentBody) throws Exception {
         String name = tree.getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
         // System.out.println(name);
         switch (tree.getChildCount()) {
             case 1: {
                 switch (tree.getChild(0).getClass().getSimpleName().replaceAll("Context$", "").toLowerCase()) {
                     case "literal": {
-                        return parseLiteral(tree.getChild(0));
+                        return parseLiteral(tree.getChild(0), parentBody);
                     }
                     case "read_int": {
                         return new ReadInt();
@@ -250,18 +250,18 @@ public class Parser {
                         return new ReadString();
                     }
                     case "function_literal":{
-                        return parseFunctionLiteral(tree.getChild(0));
+                        return parseFunctionLiteral(tree.getChild(0), parentBody);
                     }
                 }
             }
             case 3: {
-                return parseExpression(tree.getChild(1));
+                return parseExpression(tree.getChild(1), parentBody);
             }
         }
         return new Empty();
     }
 
-    private Expression parseFunctionLiteral(ParseTree tree) throws Exception {
+    private Expression parseFunctionLiteral(ParseTree tree, Body parentBody) throws Exception {
         String name = tree.getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
         // System.out.println(name);
         ArrayList<IDENT> paramNames = new ArrayList<>();
@@ -269,13 +269,13 @@ public class Parser {
             paramNames.add(new IDENT(tree.getChild(i).getText()));
         }
         if (tree.getChild(tree.getChildCount() - 1).getChildCount() == 3) {
-            return new Function(paramNames, parseBody(tree.getChild(tree.getChildCount() - 1).getChild(1)), null);
+            return new Function(paramNames, parseBody(tree.getChild(tree.getChildCount() - 1).getChild(1), null), null);
         } else {
-            return new Function(paramNames, null, parseExpression(tree.getChild(tree.getChildCount() - 1).getChild(1)));
+            return new Function(paramNames, null, parseExpression(tree.getChild(tree.getChildCount() - 1).getChild(1), null));
         }
     }
 
-    private Expression parseLiteral(ParseTree tree) throws Exception {
+    private Expression parseLiteral(ParseTree tree, Body parentBody) throws Exception {
         String name = tree.getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
         // System.out.println(name);
 
@@ -304,7 +304,7 @@ public class Parser {
         } else {
             switch (tree.getChild(0).getClass().getSimpleName().replaceAll("Context$", "").toLowerCase()) {
                 case "array": {
-                    return parseArray(tree.getChild(0));
+                    return parseArray(tree.getChild(0), parentBody);
                 }
                 case "tuple": {
                     tree = tree.getChild(0);
@@ -313,10 +313,10 @@ public class Parser {
                     for (int i = 1; i < tree.getChildCount(); i += 2) {
                         if (tree.getChild(i).getChildCount() == 1) {
                             idents.add(null);
-                            exps.add(parseExpression(tree.getChild(i).getChild(0)));
+                            exps.add(parseExpression(tree.getChild(i).getChild(0), parentBody));
                         } else {
                             idents.add(new IDENT(tree.getChild(i).getChild(0).getText()));
-                            exps.add(parseExpression(tree.getChild(i).getChild(2)));
+                            exps.add(parseExpression(tree.getChild(i).getChild(2), parentBody));
                         }
                     }
                     return new Tuple(idents, exps);
@@ -326,12 +326,12 @@ public class Parser {
         return new Empty();
     }
 
-    private Expression parseArray(ParseTree tree) throws Exception {
+    private Expression parseArray(ParseTree tree, Body parentBody) throws Exception {
         String name = tree.getClass().getSimpleName().replaceAll("Context$", "").toLowerCase();
         // System.out.println(name);
         ArrayList<Expression> exps = new ArrayList<>();
         for (int i = 1; i < tree.getChildCount() - 1; i += 2) {
-            exps.add(parseExpression(tree.getChild(i)));
+            exps.add(parseExpression(tree.getChild(i), parentBody));
         }
         return new Array(exps);
     }
